67770c5b8fc2b3ad4937d3099dfe557eb7bc830a
Ram Pandey <8826047538r@gmail.com>
2023-10-02 09:27:42 +0530

    lint updated

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  crow::SimpleApp app;
+    crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)(
-      []() { return "<div><h1>Hello, World.</h1></div>"; });
+    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)(
+        []() { return "<div><h1>Hello, World.</h1></div>"; });
 
-  char *port = getenv("PORT");
-  uint16_t iPort =
-      static_cast<uint16_t>(port != NULL ? std::stoi(port) : 18080);
-  CROW_LOG_INFO << "App running on port: " << iPort;
-  app.port(iPort).multithreaded().run();
+    char    *port = getenv("PORT");
+    uint16_t iPort =
+        static_cast<uint16_t>(port != NULL ? std::stoi(port) : 18080);
+    CROW_LOG_INFO << "App running on port: " << iPort;
+    app.port(iPort).multithreaded().run();
 }
