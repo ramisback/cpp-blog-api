bc3a9c4ed39f850f7b3263f42ea821fe29e21c37
Ram Pandey <8826047538r@gmail.com>
2023-10-02 12:32:04 +0530

    lint

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
         []() { return "<div><h1>Hello, World.</h1></div>"; });
 
     char *port = getenv("PORT");
+
     uint16_t iPort = static_cast<uint16_t>(port ? std::stoi(port) : 18080);
     CROW_LOG_INFO << "App running on port: " << iPort;
     app.port(iPort).multithreaded().run();
