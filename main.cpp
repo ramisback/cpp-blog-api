39bdaab94b3a3d65a4945a5e69f7013b15a392db
Ankush Ojha <alpha98689@gmail.com>
2023-10-07 14:04:29 +0000

    fix: lint issues

--- main.cpp
+++ main.cpp
@@ -2,20 +2,13 @@
 #define CROW_H
 #include "include/crow_all.h"
 #endif
-#include "src/hello.h"
+#include "./src/api/routes.h"
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[]) {
     crow::SimpleApp app;
-
-    std::function<std::string()> respondWithGreeting = []() {
-        std::string greeting = hello("Hello, World!");
-        return "<div><h1>" + greeting + "</h1></div>";
-    };
-
-    CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)(respondWithGreeting);
-
+    routes(app);
     char    *port = getenv("PORT");
     uint16_t iPort =
         static_cast<uint16_t>(port != NULL ? std::stoi(port) : 18080);
--- src/api/routes.cpp
+++ src/api/routes.cpp
@@ -5,30 +5,30 @@
 #include "routes.h"
 #include <iostream>
 
-void routes(crow::SimpleApp &app){
+void routes(crow::SimpleApp &app) {
 
     CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)([]() {
         std::string greeting = "Hello, World!";
         return "<div><h1>" + greeting + "</h1></div>";
     });
 
-    //Health Check Endpoint
-    CROW_ROUTE(app, "/health").methods(crow::HTTPMethod::GET)([](){
+    // Health Check Endpoint
+    CROW_ROUTE(app, "/health").methods(crow::HTTPMethod::GET)([]() {
         crow::json::wvalue response;
-        std::string responseTime = "15ms";
+        std::string        responseTime = "15ms";
 
         bool databaseCheck = false;
 
-        response["details"]["database"]["status"] = databaseCheck ? "up" : "down";
+        response["details"]["database"]["status"] =
+            databaseCheck ? "up" : "down";
         response["details"]["database"]["response_time"] = responseTime;
-        response["status"] = "serving";
+        response["status"]                               = "serving";
 
-        char buffer[25];
+        char   buffer[25];
         time_t t = time(nullptr);
         strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H:%M:%SZ", gmtime(&t));
         response["timestamp"] = buffer;
 
         return response;
-
     });
 }
